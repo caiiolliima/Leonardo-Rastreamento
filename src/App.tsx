140035e0c24e5a904f09e96b77ff9cd75ed7d12b
Caio Lima <lima.caioalves@gmail.com>
2024-07-28 18:38:22 -0300

    COMMIT

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+    <div className="m-56">
+        <h1 className="text-8xl">
+          Rastreamento
+        </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.tsx'
+import './global.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+    </Routes>
+  </BrowserRouter>,
+)
