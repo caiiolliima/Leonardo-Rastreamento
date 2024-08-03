a4d2ec8a38804c0eaa1b4064409e973ad569e5f3
Caio Lima <lima.caioalves@gmail.com>
2024-08-02 23:22:50 -0300

    c

--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -9,15 +9,15 @@ const SideBar = () => {
   ]
 
   return (
-    <div className='shadow-lg min-w-dvh'>
-      <div className='md:px-10 py-4 px-7'>
+    <div className='shadow-lg min-w-dvh top-0 left-0'>
+      <div className='md:px-10 py-4 px-7 md:flex justify-between items-center bg-white'>
         <div className='flex gap-4 text-2xl items-center'>
         <img src='/src/images/logo.svg' className='w-12 h-12'/>
           <span className='font-bold'>NerdBot Rastreio</span>
         </div>
-        <ul className="flex flex-wrap gap-2">
+        <ul className="md:flex pl-9 md:pl-0">
           {
-            links.map(link => <li><a href="/">{link.name}</a></li>)
+            links.map(link => <li className="font-semibold my-7 md:my-0 md:ml-8"><a href="/">{link.name}</a></li>)
           }
         </ul>
       </div>
