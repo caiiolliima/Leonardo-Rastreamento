efe951739ddf2c68010da6e23664e884a9553f0a
Caio Lima <lima.caioalves@gmail.com>
2024-08-07 06:24:22 -0300

    modified:   src/App.tsx modified:   src/Components/SideBar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ function App() {
   return (
     <>
       <SideBar />
-      <div className='flex flex-col justify-center bg-blue-200'>
+      <div className='flex flex-col justify-center bg-blue-100'>
         <div className='flex justify-center align-center h-48 mt-10'>
           <img src='/src/images/logo.svg' />
         </div>
         <div className='place-self-center mt-5'>
-        <Theme radius="large" accentColor='blue' className='bg-blue-200 w-96'>
+        <Theme radius="large" accentColor='blue' className='bg-blue-100 w-96 z-10'>
             <TextField.Root size="3" placeholder="Insira o código de rastreio...">
               <TextField.Slot side="right" px="1">
                 <Button size="2">
--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -1,24 +1,38 @@
+import { Bars3BottomRightIcon, XMarkIcon } from "@heroicons/react/16/solid";
+import { useState } from "react";
+
 const SideBar = () => {
 
   const links = [
-    {name: "Login", href:"/"},
     {name: "Cadastro", href:"/"},
     {name: "Perfil", href:"/"},
     {name: "Logout", href:"/"},
     {name: "Meu Rastreio", href:"/"},
   ]
 
+  const [isOpen, setisOpen] = useState(false)
+
   return (
-    <div className='shadow-lg min-w-dvh top-0 left-0'>
+    <div className='shadow-lg w-screen top-0 left-0'>
       <div className='md:px-10 py-4 px-7 md:flex justify-between items-center bg-white'>
         <div className='flex gap-4 text-2xl items-center'>
         <img src='/src/images/logo.svg' className='w-12 h-12'/>
           <span className='font-bold'>NerdBot Rastreio</span>
         </div>
+
+        <div  onClick={() => setisOpen(!isOpen)} className={`w-7 h-7 absolute right-8 top-6 cursor-pointer md:hidden`}>
+          {
+            isOpen ? <XMarkIcon /> : <Bars3BottomRightIcon />
+          }
+        </div>
+
         <ul className="md:flex pl-9 md:pl-0">
           {
             links.map(link => <li className="font-semibold my-7 md:my-0 md:ml-8"><a href="/">{link.name}</a></li>)
           }
+
+          <button className="btn bg-blue-600 text-white py-1 px-3 md:ml-8 rounded md:static">Login</button>
+
         </ul>
       </div>
     </div>
